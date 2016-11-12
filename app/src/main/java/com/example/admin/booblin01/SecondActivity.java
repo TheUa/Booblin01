ea9c69dd52ede3f31486bbd13b33f5926e0a1686
TheUa <the.dn.ua@gmail.com>
2016-11-12 11:52:46 +0200

    none

--- app/src/main/java/com/example/admin/booblin01/SecondActivity.java
+++ app/src/main/java/com/example/admin/booblin01/SecondActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,6 +19,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -71,6 +76,15 @@ public class SecondActivity extends AppCompatActivity implements WheelPicker.OnI
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         final CollapsingToolbarLayout collapsingToolbarLayout =
                 (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
@@ -916,4 +930,39 @@ public class SecondActivity extends AppCompatActivity implements WheelPicker.OnI
     @Override
     public void onItemSelected(WheelPicker picker, Object data, int position) {
     }
+@SuppressWarnings("StatementWithEmptyBody")
+@Override
+public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_menu) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        }
+        if (id == R.id.nav_add) {
+
+        } else if (id == R.id.nav_coctail) {
+        Toast.makeText(this, "В разработке", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_dolg) {
+        Toast.makeText(this, "В разработке", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_share) {
+        Toast.makeText(this, "В разработке", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_send) {
+        Toast.makeText(this, "В разработке", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+        }
+
+@Override
+public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        drawer.closeDrawer(GravityCompat.START);
+        } else {
+        super.onBackPressed();
+        }
 }
